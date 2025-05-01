54305ff9b4fc7f1840032682bf0f06bd33ded251
rjrizani <rjrizani@gmail.com>
2025-05-01 19:04:59 +0800

    better ouput with data cleaner in pipeline

--- marketplace/marketplace/pipelines.py
+++ marketplace/marketplace/pipelines.py
@@ -15,11 +15,33 @@ class MarketplacePipeline:
 class snooperPipeline:
     def process_item(self, item, spider):
         item = dict(item)
+        """
         item['product_price'] = item['product_price'].strip() if item['product_price'] else None
         item['price_w_discount'] = item['price_w_discount'].strip() if item['price_w_discount'] else None
 
         item['original_price'] = item['original_price'].strip() if item['original_price'] else None
+        """
+        item['product_price'] = self.convert_price_to_float(item['product_price'])
+        item['price_w_discount'] = self.convert_price_to_float(item['price_w_discount'])
+
+        item['original_price'] = self.convert_price_to_float(item['original_price'])
         return item
+    
+    import pandas as pd
+
+    def convert_price_to_float(self,price_string):
+       
+        if not isinstance(price_string, str):
+            return None  # or return the original value
+
+        try:
+            # Remove currency symbols, commas, and other non-numeric characters
+            cleaned_price = ''.join(c for c in price_string if c.isdigit() or c == '.')
+
+            # Convert to float
+            return float(cleaned_price)
+        except ValueError:
+            return None  # or return the original value
 
 class firstcryPipeline:
     def process_item(self, item, spider):
